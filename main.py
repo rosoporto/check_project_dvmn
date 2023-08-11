dfbea778c3d83953b61dbea89810bffbcd1c9924
Aleksey <rosoporto@yandex.ru>
2023-08-12 00:08:05 +0300

    catch errors and write to log. Pure code

--- main.py
+++ main.py
@@ -26,10 +26,8 @@ class TgLogsHandler(logging.Handler):
 def main():
     load_dotenv()
     
-    devman_token = os.environ.get('DEVMAN_TOKEN')
     tg_token_bot = os.environ.get('TG_TOKEN_BOT')
-    tg_chat_id = os.environ.get('TG_CHAT_ID')    
-    url = 'https://dvmn.org/api/long_polling/'
+    tg_chat_id = os.environ.get('TG_CHAT_ID')
     
     logging.basicConfig(
         format='%(levelname)s:%(name)s:%(message)s',
@@ -37,47 +35,14 @@ def main():
     )    
     tg_handler = TgLogsHandler(tg_token_bot, tg_chat_id)
     logger.addHandler(tg_handler)
-
-    headers = {'Authorization': f'Token {devman_token}'}
-    last_time = ''
-    
+  
     logger.info('Bot started')
     
     try:
         1/0
-    except Exception:
-        logger.exception()
+    except ZeroDivisionError:
+        logger.exception('Deliberate divide by zero traceback')
         
-    while True:
-        try:
-            params = {'timestamp': last_time}
-            response = requests.get(url, params=params, headers=headers)
-            response.raise_for_status()
-            review = response.json()
-            if review['status'] == 'found':
-                answer = review['new_attempts'][0]
-                if answer['is_negative']:
-                    status = 'В работе есть ошибки'
-                else:
-                    status = 'Работа проверена. Ошибок нет'
-                lesson_title = answer['lesson_title']
-                lesson_url = answer['lesson_url']             
-            
-                last_time = review.get('last_attempt_timestamp', last_time)
-                
-                tg_handler.bot.send_message(chat_id=tg_chat_id,
-                                text=(f'У вас проверили работу "{lesson_title}"\n\n'
-                                    f'{status}\n{lesson_url}'))
-            else:
-                last_time = review.get('timestamp_to_request', last_time)
-                    
-        except requests.exceptions.ReadTimeout:
-            logger.error('Time Out')
-            continue
-        except requests.exceptions.ConnectionError:
-            logger.error('Lost connection')
-            time.sleep(600)
-
 
 if __name__ == '__main__':
     main()
